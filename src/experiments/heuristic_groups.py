7e827104834931cbe01602c87312e66a200cb0ac
ohadnir31-cmyk <ohadnir31@gmail.com>
2026-05-04 11:25:28 +0300

    Create heuristic_groups.py

---
+++ src/experiments/heuristic_groups.py
@@ -0,0 +1,18 @@
+HEURISTIC_TO_GROUP = {
+    # Feasibility
+    "MPS": "Feasible",
+    "FNI": "Feasible",
+    "FMTTB": "Feasible",
+
+    # Ratio / urgency
+    "Ratio": "Ratio",
+    "Danger": "Ratio",
+    "Lookahead": "Ratio",
+
+    # Spatial
+    "Cluster": "Spatial",
+
+    # Proximity / weighted
+    "NI": "Proximity",
+    "Weighted": "Proximity",
+}
