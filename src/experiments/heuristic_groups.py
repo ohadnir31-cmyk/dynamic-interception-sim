b25c1961e4292745d555c9d5cf5ca240c73bd3c2
ohadnir31-cmyk <ohadnir31@gmail.com>
2026-05-04 14:38:56 +0300

    Update heuristic_groups.py

--- src/experiments/heuristic_groups.py
+++ src/experiments/heuristic_groups.py
@@ -1,20 +1,35 @@
+# from __future__ import annotations
+
+# HEURISTIC_TO_GROUP = {
+#     # Feasibility-oriented policies
+#     "MPS": "Feasible",
+#     "FNI": "Feasible",
+#     "FMTTB": "Feasible",
+
+#     # Ratio / urgency / composite urgency policies
+#     "Ratio": "RatioUrgency",
+#     "Danger": "RatioUrgency",
+#     "Lookahead": "RatioUrgency",
+
+#     # Spatial policy
+#     "Cluster": "Spatial",
+
+#     # Proximity / weighted policies
+#     "NI": "Proximity",
+#     "Weighted": "Proximity",
+# }
+
+
 from __future__ import annotations
 
 HEURISTIC_TO_GROUP = {
-    # Feasibility-oriented policies
     "MPS": "Feasible",
     "FNI": "Feasible",
-    "FMTTB": "Feasible",
 
-    # Ratio / urgency / composite urgency policies
     "Ratio": "RatioUrgency",
     "Danger": "RatioUrgency",
-    "Lookahead": "RatioUrgency",
 
-    # Spatial policy
     "Cluster": "Spatial",
 
-    # Proximity / weighted policies
     "NI": "Proximity",
-    "Weighted": "Proximity",
 }
