77f88c5e5ab41b7960bda06e477f58e1b21d0bfa
ohadnir31-cmyk <ohadnir31@gmail.com>
2026-06-18 17:57:23 +0300

    Update heuristic_groups.py

--- src/experiments/heuristic_groups.py
+++ src/experiments/heuristic_groups.py
@@ -1,50 +1,13 @@
-# from __future__ import annotations
-
-# HEURISTIC_TO_GROUP = {
-#     # Feasibility-oriented policies
-#     "MPS": "Feasible",
-#     "FNI": "Feasible",
-#     "FMTTB": "Feasible",
-
-#     # Ratio / urgency / composite urgency policies
-#     "Ratio": "RatioUrgency",
-#     "Danger": "RatioUrgency",
-#     "Lookahead": "RatioUrgency",
-
-#     # Spatial policy
-#     "Cluster": "Spatial",
-
-#     # Proximity / weighted policies
-#     "NI": "Proximity",
-#     "Weighted": "Proximity",
-# }
-
-
-# from __future__ import annotations
-
-# HEURISTIC_TO_GROUP = {
-#     "MPS": "Feasible",
-#     "FNI": "Feasible",
-
-#     "Ratio": "RatioUrgency",
-#     "Danger": "RatioUrgency",
-
-#     "Cluster": "Spatial",
-
-#     "NI": "Proximity",
-# }
-
-
 from __future__ import annotations
 
+# Coarse grouping used by older exploratory group-classification experiments.
+# The main proposal-level portfolio is:
+# NI, FNI, FMTTB, MPS, FCluster.
+
 HEURISTIC_TO_GROUP = {
-    "MPS": "Feasible",
-    "FNI": "Feasible",
-
-    "Ratio": "RatioUrgency",
-    "Danger": "RatioUrgency",
-
-    "Cluster": "Spatial",
-
     "NI": "Proximity",
+    "FNI": "FeasibleProximity",
+    "FMTTB": "FeasibleUrgency",
+    "MPS": "FeasibleSlack",
+    "FCluster": "SpatialPositioning",
 }
